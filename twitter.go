6867127bd48cfffa4948d68ae350feb9c16c07c4
Onteria <onteria@kogasa.jp>
2012-11-05 06:56:33 -0800

    Adding an api get method for doing base GET API calls. Will have to figure a way out to consolidate this with the post method later. For now just get it up and working.

--- twitter.go
+++ twitter.go
@@ -2,12 +2,18 @@ package ntweet
 
 import (
   "github.com/garyburd/go-oauth/oauth"
+  "fmt"
+  "encoding/json"
+  "net/url"
+  "net/http"
+  "io/ioutil"
 )
 
 const (
   RequestUri = "https://api.twitter.com/oauth/request_token"
   AuthorizationUri = "https://api.twitter.com/oauth/authorize"
   TokenUri = "https://api.twitter.com/oauth/access_token"
+  ApiUri = "https://api.twitter.com/1.1/"
 )
 
 type TwitterClient struct {
@@ -35,4 +41,27 @@ func NewClient(ckey string, csecret string, token string, secret string) *Twitte
     OauthClient: oauthClient,
     TokenCredentials: tokenInfo,
   }
+}
+
+func (c *TwitterClient) apiGetCall(resource string, params url.Values, data interface{}) error{
+  if params == nil {
+    params = make(url.Values)
+  }
+
+  urlStr := ApiUri + resource
+
+  c.OauthClient.SignParam(&c.TokenCredentials, "GET", urlStr, params)
+  resp, err := http.Get( urlStr + "?" + params.Encode())
+  if err != nil {
+    return err
+  }
+
+  defer resp.Body.Close()
+
+  if resp.StatusCode != 200 {
+    p, _ := ioutil.ReadAll(resp.Body)
+    return fmt.Errorf("Get %s returned status %d, %s", urlStr, resp.StatusCode, p)
+  }
+
+  return json.NewDecoder(resp.Body).Decode(data)
 }
